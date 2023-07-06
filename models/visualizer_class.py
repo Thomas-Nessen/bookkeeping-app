3f3a046ab5a46c2bbda705d7b2c89db847354e1f
Thomas Nessen <80414448+Thomas-Nessen@users.noreply.github.com>
2023-07-06 13:22:36 +0200

    plotly express barchart

--- models/visualizer_class.py
+++ models/visualizer_class.py
@@ -1,6 +1,10 @@
 import plotly.graph_objects as go
+import plotly.express as px
+from datetime import timedelta
+import pandas as pd
+import numpy as np
 
-class DisplayClass(object):
+class VisualizerClass(object):
     """
     This class is used to visualize the transaction data to the user.
     It includes functions for:
@@ -9,36 +13,65 @@ class DisplayClass(object):
         - Single transactions
         
     """
-    def __init__(self, cat_data):
-        self.data = cat_data
+    def __init__(self):
+    # def __init__(self, cat_data):
+        # self.data = cat_data
+        self.colours = [
+            "#ff6961", "#ffb480", "#f8f38d", "#42d6a4",
+            "#08cad1", "#59adf6", "#9d94ff", "#c780e8"
+            ] 
 
     
-    def bar_graph(self, data):
-        # pass
+    def bar_graph_categories(self, data, x_column:str , y_column:str, colours=None):
+        # use default colours if not given
+        colours = self.colours if colours == None else colours
 
-        #  Get a convenient list of x-values
-        years = data['year']
-        x = list(range(len(years)))
+        try:
+            # create dict with total sum of transactions per category
+            cats = data[x_column].unique()    # Could also get these from sorting codes?
+            vis_data = {}
+            for cat in cats:
+                #set category as key, select corresponding rows, set value as rounded sum of the 'Amount' column
+                vis_data[cat] = data[data[x_column] == cat][y_column].sum().round(2)
+        except AttributeError as e: 
+            print(e)
+            print("The data types of one or more of the columns is incorrect.")
+        except KeyError as e:
+            print(e)
+            print("One or more of the columns given does not exist")
 
-        # Specify the plots
-        bar_plots = [
-            go.Bar(x=x, y=data['conservative'], name='Conservative', marker=go.bar.Marker(color='#0343df')),
-            go.Bar(x=x, y=data['labour'], name='Labour', marker=go.bar.Marker(color='#e50000')),
-            go.Bar(x=x, y=data['liberal'], name='Liberal', marker=go.bar.Marker(color='#ffff14')),
-            go.Bar(x=x, y=data['others'], name='Others', marker=go.bar.Marker(color='#929591')),
-        ]
-
-        # Customise some display properties
-        layout = go.Layout(
-            title=go.layout.Title(text="Election results", x=0.5),
-            yaxis_title="Seats",
-            xaxis_tickmode="array",
-            xaxis_tickvals=list(range(27)),
-            xaxis_ticktext=tuple(data['year'].values),
+        fig = go.Figure(
+            data=[go.Bar(
+                x=list(vis_data.keys()),
+                y=list(vis_data.values()),
+                title = "Hoi",
+                marker_color=colours)
+            ]
         )
 
-        # Make the multi-bar plot
-        fig = go.Figure(data=bar_plots, layout=layout)
-
-        # Tell Plotly to render it
         fig.show()
+        
+    def stacked_bar_graph(self, data):
+        fig = px.bar(
+            data, x="Date", y="Amount", color="Category",
+            hover_name=data["Name / Description"]) #, Tag="Tag"))
+
+        fig.update_layout(
+            title_text = "Daily expenses overview",
+            colorway = self.colours
+        )
+        fig.update_xaxes(
+            type='date',
+            dtick = 86400000,
+            range = [
+                data['Date'].min() - timedelta(days=1),
+                data['Date'].max() + timedelta(days=1)
+                ]
+            )
+        fig.update_yaxes(tick0 = 0)
+        
+        fig.show()
+
+    if __name__ == "__main__":
+        stacked_bar_graph()
+
